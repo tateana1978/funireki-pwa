1df46e730f2d6b9a451552eee2f4bffbf7e4c19a
油山山麓古墳群（Fukuoka） <146042477+tateana1978@users.noreply.github.com>
2025-11-03 17:39:03 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,20 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('funireki-cache-v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-});
-
-self.addEventListener('fetch', (event) => {
-  // すべてのリクエストはネットワーク経由（オフラインキャッシュなし）
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
